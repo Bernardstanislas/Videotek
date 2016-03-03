5c79d8e8f6877b67ca68593ec0c3ba0795e4e498
Stanislas Bernard <bernardstanislas@gmail.com>
2016-03-03 01:09:58 +0100

    Remove leveldb, add pouchdb

--- src/database.js
+++ src/database.js
@@ -1,51 +1,38 @@
-"use strict";
+import fetch from 'isomorphic-fetch';
+import PouchDB from 'pouchdb';
+import Promise from 'bluebird';
+import 'babel-polyfill';
 
-const fetch = require('isomorphic-fetch');
-const levelup = require('levelup');
-const Promise = require('bluebird');
-
+PouchDB.debug.enable('*');
 const MOVIES_PER_PAGE = 50;
 const YTS_API = 'https://yts.ag/api/v2/';
 
-const db = levelup('./db');
+const db = new PouchDB('videotek');
 
-db.get = Promise.promisify(db.get);
-
-const refresh = () => {
-    fetch(`${YTS_API}list_movies.json?limit=1`)
-    .then(raw => raw.json())
-    .then(response => Promise.resolve(response.data.movie_count))
-    .then(size => Math.ceil(size / MOVIES_PER_PAGE))
-    .then(pages => {
-        const pagesToFetch = [];
-        for (let pageIndex = 1; pageIndex <= pages; pageIndex++)
-            pagesToFetch.push(pageIndex);
-        return pagesToFetch;
-    })
-    .then(pagesToFetch => Promise.mapSeries(pagesToFetch, page => {
-        console.log(`Fetching page ${page}/${pagesToFetch.length}`);
-        return fetch(`${YTS_API}list_movies.json?page=${page}&limit=${MOVIES_PER_PAGE}`)
-        .then(raw => raw.json())
-        .then(response => response.data.movies)
-    }))
-    .then(moviesArray => moviesArray.reduce((acc, movies) => acc.concat(movies), []))
-    .then(movies => movies.map(movie => ({
-        type: 'put',
-        key: movie.id,
-        value: JSON.stringify(movie)
-    })))
-    .then(batch => db.batch(batch, error => {
-    }))
+const refresh = async () => {
+    const raw = await fetch(`${YTS_API}list_movies.json?limit=1`);
+    const {data: {movie_count: movieCount}} = await raw.json();
+    const pagesCount = Math.ceil(movieCount / MOVIES_PER_PAGE);
+    const pagesArray = Array.from(Array(pagesCount).keys()); // © @pierr
+    const moviesArray = await Promise.mapSeries(pagesArray, async (page) => {
+        console.log(`Fetching page ${page}/${pagesArray.length}`);
+        const raw = await fetch(`${YTS_API}list_movies.json?page=${page}&limit=${MOVIES_PER_PAGE}`);
+        const {data: {movies}} = await raw.json();
+        return movies;
+    });
+    const movies = moviesArray.reduce((acc, movies) => acc.concat(movies), []);
+    const moviesBatch = movies.map(({id, ...movie}) => ({
+        _id: id.toString(),
+        ...movie
+    }));
+    await db.bulkDocs(moviesBatch);
+    console.log('Database refreshed !');
 }
 
 const getMovie = id => db.get(id)
-.then(JSON.parse)
 
-const getAllMovies = () => new Promise((resolve, reject) => {
-    const buffer = [];
-    db.createValueStream()
-    .on('data', value => buffer.push(JSON.parse(value)))
-    .on('end', () => resolve(buffer))
+const getAllMovies = () => db.allDocs({
+  include_docs: true
 })
 
 module.exports = {
