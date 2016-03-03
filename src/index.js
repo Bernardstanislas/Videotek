45501b803d5f6f2320f385fda3bd008666821191
Stanislas Bernard <bernardstanislas@gmail.com>
2016-03-03 01:10:17 +0100

    Add Electron

--- src/index.js
+++ src/index.js
@@ -1,42 +1,33 @@
-const express = require('express');
+'use strict';
 
-const database = require('./database');
-const searchIndex = require('./search');
+import React, {Component} from 'react';
+import ReactDom from 'react-dom';
+import {getMovie, getAllMovies, refresh} from './database';
 
-const app = express();
+class App extends Component {
+    state = {movie: {}, movieCount: 0};
 
+    componentDidMount() {
+        getMovie('10')
+        .then(movie => {
+            getAllMovies()
+            .then(movies => movies.total_rows)
+            .then(movieCount => this.setState({movie, movieCount}))
+        })
+    }
 
-app.post('/database/refresh', (req, res) => {
-    database.refresh();
-    res.send('Refreshing the database');
-})
+    render() {
+        const {movie, movieCount} = this.state;
+        return (
+            <div>
+                <button onClick={refresh}>Refresh database</button>
+                <i>{movieCount} in base</i>
+                <h1>{movie.title}</h1>
+                <img src={movie.medium_cover_image}/>
+                <p>{JSON.stringify(movie)}</p>
+            </div>
+        );
+    }
+}
 
-app.get('/movies/:id', (req, res) => {
-    database.getMovie(req.params.id)
-    .then(movie => res.json(movie))
-    .catch(error => res.status(404).send('Movie not found'));
-})
-
-app.get('/movies', (req, res) => {
-    database.getAllMovies()
-    .then(movies => res.json(movies))
-    .catch(error => res.status(500).json(error));
-})
-
-app.get('/search/populate', (req, res) => {
-    searchIndex.populate();
-    res.send('Populating the search index..')
-})
-
-app.get('/search/:query', (req, res) => {
-    searchIndex.search(req.params.query)
-    .then(results => res.json(results))
-    .catch(error => res.status(500).json(error));
-})
-
-searchIndex.init
-.then(() => {
-    app.listen(3000, () => {
-        console.log('API listening on port 3000');
-    });
-})
+ReactDom.render(<App/>, document.getElementById('react-root'));
